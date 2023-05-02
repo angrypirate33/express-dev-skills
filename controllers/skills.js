953f94c667a445b75e2113db71b10d76c6e7e1df
Jon Schiavone <jonschiavoneucf@gmail.com>
2023-05-02 19:44:12 -0400

    Adds skills dataset.

---
+++ controllers/skills.js
@@ -0,0 +1,25 @@
+const skills = [
+    {
+        name: 'coding',
+        type: 'hard',
+        level: 'intermediate'
+    }, 
+    {
+        name: 'problem solving',
+        type: 'soft',
+        level: 'advanced'
+    },
+    {
+        name: 'teamwork',
+        type: 'soft',
+        level: 'beginner'
+    }
+]
+
+function getAll() {
+    return skills
+}
+
+function getOne(name) {
+    return skills.find(skill => skill.name === name)
+}
